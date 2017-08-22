cd68df6980fa38755a694cb6cc47c5764ccfdac1
Yasser Rachid <anuar.yasser@gmail.com>
2017-08-22 16:32:35 -0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'notes',
+  packages = ['notes'],
+  version = '0.1',
+  description = 'Manage txt files',
+  author = 'Yasser Rachid',
+  author_email = 'anuar.yasser@gmail.com',
+  url = 'https://github.com/othman853/mypackage',
+  download_url = 'https://github.com/othman853/notes/archive/0.1.tar.gz',
+  keywords = ['notes'],
+  classifiers = [],
+)
